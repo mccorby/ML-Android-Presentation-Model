428fcf7fc1c7dd66a7c32c90a1ad831a6ef07faa
Jose Corbacho <JoseAntonio.Corbacho@sky.uk>
2017-08-22 07:38:28 +0100

    Added summaries for TensorBoard

--- convnet.py
+++ convnet.py
@@ -58,7 +58,8 @@ def deepnn(x):
     W_conv1 = weight_variable([5, 5, 1, 32])
     b_conv1 = bias_variable([32])
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-
+    variable_summaries(W_conv1)
+    variable_summaries(b_conv1)
     # Pooling layer - downsamples by 2X.
     h_pool1 = max_pool_2x2(h_conv1)
 
@@ -66,6 +67,8 @@ def deepnn(x):
     W_conv2 = weight_variable([5, 5, 32, 64])
     b_conv2 = bias_variable([64])
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    variable_summaries(W_conv2)
+    variable_summaries(b_conv2)
 
     # Second pooling layer.
     h_pool2 = max_pool_2x2(h_conv2)
@@ -74,6 +77,8 @@ def deepnn(x):
     # is down to 7x7x64 feature maps -- maps this to 1024 features.
     W_fc1 = weight_variable([7 * 7 * 64, 1024])
     b_fc1 = bias_variable([1024])
+    variable_summaries(W_fc1)
+    variable_summaries(b_fc1)
 
     h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
@@ -82,10 +87,13 @@ def deepnn(x):
     # features.
     keep_prob = tf.placeholder(tf.float32, name='dropout_prob')
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    tf.summary.scalar('dropout_keep_probability', keep_prob)
 
     # Map the 1024 features to 10 classes, one for each digit
     W_fc2 = weight_variable([1024, 10])
     b_fc2 = bias_variable([10])
+    variable_summaries(W_fc2)
+    variable_summaries(b_fc2)
 
     y_conv = tf.add(tf.matmul(h_fc1_drop, W_fc2), b_fc2, name='output_node')
     return y_conv, keep_prob
@@ -130,18 +138,29 @@ def main(_):
         y_conv, keep_prob = deepnn(x)
 
         cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+        tf.summary.scalar('cross_entropy', cross_entropy)
         train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
         correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar('accuracy', accuracy)
 
     with tf.Session(graph=graph) as sess:
+        # Merge all the summaries and write them out to /tmp/mnist_logs (by default)
+        merged = tf.summary.merge_all()
+        train_writer = tf.summary.FileWriter('summaries' + '/train', sess.graph)
+        test_writer = tf.summary.FileWriter('summaries' + '/test')
         sess.run(tf.global_variables_initializer())
-        for i in range(15000):
+        for i in range(5000):
             batch = mnist.train.next_batch(50)
             if i % 100 == 0:
-                train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+                summary, train_accuracy = sess.run([merged, accuracy], feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+                # summary, train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
                 print('step %d, training accuracy %g' % (i, train_accuracy))
-            train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+                test_writer.add_summary(summary, i)
+            else:
+                summary, _ = sess.run([merged, train_step], feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+                # train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+                train_writer.add_summary(summary, i)
 
         print('test accuracy %g' % accuracy.eval(feed_dict={
             x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
@@ -150,6 +169,20 @@ def main(_):
         saver = tf.train.Saver()
         # Save variables to .ckpt file
         saver.save(sess, "./runs/trained_variables.ckpt")
+
+
+def variable_summaries(var):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
